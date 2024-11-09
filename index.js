5120eb2d52821193e5865f12a2b4e0a44f15743e
CodeLeiZ <128136623+CodeLeiZ@users.noreply.github.com>
2024-11-09 16:22:29 -0500

    Add renderLead function

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-let myLeads = []
+let myLeads = [];
 
-const inputEl = document.getElementById("input-el");
+const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value = ""
 })
+
+function renderLeads() {
+    let itemList = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        itemList += "<li><a href='"+myLeads[i]+"' target='_blank'>" + myLeads[i] + "</a></li>"
+    }
+    ulEl.innerHTML = itemList
+}
+
 
