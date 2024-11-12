eddaf1167143c7517836c78fe11e054d4ef4dc4e
CodeLeiZ <128136623+CodeLeiZ@users.noreply.github.com>
2024-11-12 09:06:07 -0500

    Add localStorage.setItem()

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el')
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+
 inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value)
-    renderLeads()
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads()
 })
 
 function renderLeads() {
